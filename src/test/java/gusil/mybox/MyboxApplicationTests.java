2a0ec923107c3694f98812e15302d2b6eb8c5958
gu-sil <jhyun5717@gmail.com>
2023-03-13 23:45:34 +0900

    feat: first commit

---
+++ src/test/java/gusil/mybox/MyboxApplicationTests.java
@@ -0,0 +1,13 @@
+package gusil.mybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6c87b76aa49ad9dadfeabf25ec36de025876624e
박종현(Park Jonghyun) <58730856+gu-sil@users.noreply.github.com>
2023-03-16 16:10:28 +0900

    feat: add assertion logic when creating directory (#5)
    - 디렉터리 Owner 존재 유무 확인
    - 디렉터리 Parent 존재 유무 확인

--- src/main/java/gusil/mybox/service/DirectoryServiceImpl.java
+++ src/main/java/gusil/mybox/service/DirectoryServiceImpl.java
@@ -2,6 +2,8 @@ package gusil.mybox.service;
 
 import gusil.mybox.dto.request.CreateDirectoryRequest;
 import gusil.mybox.dto.response.CreateDirectoryResponse;
+import gusil.mybox.exception.DirectoryNotFoundException;
+import gusil.mybox.exception.UserNotFoundException;
 import gusil.mybox.mapper.DirectoryMapper;
 import gusil.mybox.repository.DirectoryRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +14,31 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class DirectoryServiceImpl implements DirectoryService {
     private final DirectoryRepository repository;
+    private final UserService userService;
     private final DirectoryMapper mapper;
 
     @Override
     public Mono<CreateDirectoryResponse> createDirectory(CreateDirectoryRequest request) {
         return Mono
                 .just(request)
+                .flatMap(req -> userService.userExists(req.getDirectoryOwner()))
+                // Check directory owner id exists
+                .map(userExists -> {
+                    if (!userExists) {
+                        throw new UserNotFoundException(request.getDirectoryOwner());
+                    } else {
+                        return request;
+                    }
+                })
+                .flatMap(req -> repository.existsById(req.getDirectoryParent()))
+                // Check directory parent id exists
+                .map(parentExists -> {
+                    if (!parentExists && !request.getDirectoryParent().equals("root")) {
+                        throw new DirectoryNotFoundException(request.getDirectoryParent());
+                    } else {
+                        return request;
+                    }
+                })
                 .map(mapper::mapToDirectory)
                 .flatMap(repository::save)
                 .map(mapper::mapToCreateDirectoryResponse);
